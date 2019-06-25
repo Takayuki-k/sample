e2055172fe99e2b50bd81b8637ab91b368ceae03
Taka-K <55ko22ni44shi@gmail.com>
2019-06-25 12:45:33 +0900

    test

--- 03/service-worker.js
+++ 03/service-worker.js
@@ -1,31 +1,32 @@
 const CACHE_NAME = 'cache-v1';
 const urlsToCache = [
-    'index.html',
-    'css/index.css',
-    'img/bg_01.jpg',
+    '/',
+    '/index.html',
+    '/css/index.css',
+    '/img/bg_01.jpg',
     '//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
 ];
 
 // install-event
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then((cache) => {
-            console.log('Opened cache');
+            .then(function(cache) {
+                console.log('Opened cache');
 
-            // 指定されたリソースをキャッシュに追加する
-            return cache.addAll(urlsToCache);
-        })
+                  // 指定されたリソースをキャッシュに追加する
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
 // activate-event
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', function(event) {
     var cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
-        caches.keys().then((cacheNames) => {
+        caches.keys().then(function(cacheNames) {
             return Promise.all(
-                cacheNames.map((cacheName) => {
+                cacheNames.map(function(cacheName) {
                     // ホワイトリストにないキャッシュ(古いキャッシュ)は削除する
                     if (cacheWhitelist.indexOf(cacheName) === -1) {
                         return caches.delete(cacheName);
@@ -37,38 +38,38 @@ self.addEventListener('activate', (event) => {
 });
 
 // fetch-event
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
-        .then((response) => {
-            if (response) {
-                return response;
-            }
-
-            // 重要：リクエストを clone する。リクエストは Stream なので
-            // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
-            // 必要なので、リクエストは clone しないといけない
-            let fetchRequest = event.request.clone();
-
-            return fetch(fetchRequest)
-            .then((response) => {
-                if (!response || response.status !== 200 || response.type !== 'basic') {
+            .then(function(response) {
+                if (response) {
                     return response;
                 }
 
-                // 重要：レスポンスを clone する。レスポンスは Stream で
-                // ブラウザ用とキャッシュ用の2回必要。なので clone して
-                // 2つの Stream があるようにする
-                let responseToCache = response.clone();
+                  // 重要：リクエストを clone する。リクエストは Stream なので
+                  // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+                  // 必要なので、リクエストは clone しないといけない
+                let fetchRequest = event.request.clone();
 
-                caches.open(CACHE_NAME)
-                .then((cache) => {
-                    cache.put(event.request, responseToCache);
-                });
+                return fetch(fetchRequest)
+                    .then(function(response) {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
 
-                return response;
-            });
-        })
+                          // 重要：レスポンスを clone する。レスポンスは Stream で
+                          // ブラウザ用とキャッシュ用の2回必要。なので clone して
+                          // 2つの Stream があるようにする
+                        let responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                                .then(function(cache) {
+                                    cache.put(event.request, responseToCache);
+                                });
+
+                        return response;
+                    });
+            })
     );
 });
 
