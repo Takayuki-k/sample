776fa75a4bf5b036aa0a1baf5536ce1f9318ad05
Taka-K <55ko22ni44shi@gmail.com>
2019-07-05 22:22:27 +0900

    scopeChange

--- 02/navigator.js
+++ 02/navigator.js
@@ -1,9 +1,9 @@
 // service workerが有効なら、service-worker.js を登録します
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('./service-worker.js', {scope: '/02'})
+    .register('./service-worker.js', {scope: '/sample/02/'})
     .then(() => {
       // 登録成功
-    console.log('ServiceWorker registration with scope: 02');
+    console.log('ServiceWorker registration with scope: /sample/02/');
   });
 }
