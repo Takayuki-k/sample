1dac7520af6228df3e3a4bd0a9a2de126dd8bacc
Taka-K <55ko22ni44shi@gmail.com>
2019-07-05 22:34:04 +0900

    navigaterArrowFunc,registry

--- 02/navigator.js
+++ 02/navigator.js
@@ -1,9 +1,12 @@
 // service workerが有効なら、service-worker.js を登録します
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('./service-worker.js', {scope: '/sample/02/'})
-    .then(() => {
+    .register('./service-worker.js', {scope: '/02/'})
+    .then((registration) => {
       // 登録成功
-    console.log('ServiceWorker registration with scope: /sample/02/');
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+    // 登録失敗 :(
+    console.log('ServiceWorker registration failed: ', err);
   });
 }
