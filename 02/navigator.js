459c5c6a1d9f4d44334adc1986ebf4b88fcb35f9
Taka-K <55ko22ni44shi@gmail.com>
2019-11-19 21:46:14 +0900

    PWA-TEST

--- 02/navigator.js
+++ 02/navigator.js
@@ -1,10 +1,7 @@
 // service workerが有効なら、service-worker.js を登録します
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./service-worker.js')
-    .then(
-      // 登録成功
-      req => console.log('ServiceWorker registration successful: ')
-    ).catch(
+    .catch(
       // 登録失敗 :(
       err => console.log('ServiceWorker registration failed: ', err)
     );
