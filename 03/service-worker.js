04b9992625af4e42cf12a444ef49aa1e20d5b19a
Taka-K <55ko22ni44shi@gmail.com>
2019-11-28 19:46:25 +0900

    PWA

--- 03/service-worker.js
+++ 03/service-worker.js
@@ -1,77 +1,36 @@
-const CACHE_NAME = 'cache-v2';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/css/index.css',
-    '/img/bg_01.jpg',
-    '//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
+// https://qiita.com/OMOIKANESAN/items/5b23fa8ea9ea0d181df5
+
+let CACHE_NAME = 'cache-v3';
+let urlsToCache = [
+  './',
+  './index.html',
+  './css/index.css',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+  './img/bg_01.jpg',
 ];
 
 // install-event
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Opened cache');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
 
-                  // 指定されたリソースをキャッシュに追加する
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-// activate-event
-self.addEventListener('activate', function(event) {
-    var cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    // ホワイトリストにないキャッシュ(古いキャッシュ)は削除する
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+        // 指定されたリソースをキャッシュに追加する
+        return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 // fetch-event
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                if (response) {
-                    return response;
-                }
-
-                  // 重要：リクエストを clone する。リクエストは Stream なので
-                  // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
-                  // 必要なので、リクエストは clone しないといけない
-                let fetchRequest = event.request.clone();
-
-                return fetch(fetchRequest)
-                    .then(function(response) {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                          // 重要：レスポンスを clone する。レスポンスは Stream で
-                          // ブラウザ用とキャッシュ用の2回必要。なので clone して
-                          // 2つの Stream があるようにする
-                        let responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                                .then(function(cache) {
-                                    cache.put(event.request, responseToCache);
-                                });
-
-                        return response;
-                    });
-            })
-    );
-});
-
-// 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+})
