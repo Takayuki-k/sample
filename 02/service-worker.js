150dbec8f97f42e56e84295d10ec03c83eace995
Taka-K <55ko22ni44shi@gmail.com>
2019-11-28 17:21:32 +0900

    変更

--- 02/service-worker.js
+++ 02/service-worker.js
@@ -1,47 +1,47 @@
-let CACHE_NAME = 'cache-v2';
-let CACHE_KEYS = [
-  CACHE_NAME
-];
+'use strict';
+
+let CACHE_NAME = 'cache-v1';
 let urlsToCache = [
-  '/',
-  '/index.html',
-  '/about.html',
-  '/contact.html',
-  '/css/style.css',
+  './',
+  './index.html',
+  './about.html',
+  './contact.html',
+  './css/style.css',
   '//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
-  '/img/cafe--coffe01.jpg',
-  '/img/cafe--coffe02.jpg',
-  '/img/cafe--coffe03.jpg',
-  '/img/cafe--coffe04.jpg',
-  '/img/cafe--farmer01.jpg',
+  './img/cafe--coffe01.jpg',
+  './img/cafe--coffe02.jpg',
+  './img/cafe--coffe03.jpg',
+  './img/cafe--coffe04.jpg',
+  './img/cafe--farmer01.jpg',
   '//code.jquery.com/jquery-3.3.1.slim.min.js',
   '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
   '//stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
-  '/js/index.js'
+  './js/index.js'
 ];
 
 // install-event
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-        console.log('Opened cache')
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
 
         // 指定されたリソースをキャッシュに追加する
-        // return cache.addAll(urlsToCache)
-        return cache.addAll(urlsToCache.map((url) => new Request(url, {credentials: 'same-origin'})))
-      })
+      return cache.addAll(urlsToCache);
+    })
   )
 });
 
 // activate-event
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
+  let cacheWhileList = [CACHE_NAME];
+
   event.waitUntil(
-    caches.keys().then( key => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        keys.filter(keys => {
-          return !CACHE_KEYS.includes(key)
-        }).map(keys => {
-          return caches.delete(key)
+        cacheNames.map((cacheName) => {
+          if (cacheWhileList.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
       );
     })
@@ -49,51 +49,30 @@ self.addEventListener('activate', event => {
 });
 
 // fetch-event
-self.addEventListener('fetch', event => {
-  let online = navigator.online
-
-  if (online) {
-
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       if (response) {
-        return response
+        return response;
       }
 
-      // let fetchRequest = event.request.clone()
+      let fetchRequest = event.request.clone();
 
-      return fetch(event.request).then(response => {
-        cloneResponse = response.clone()
+      return fetch(fetchRequest).then((response) => {
 
-        if (response) {
-          if (response || response.status == 200) {
-            caches.open(CACHE_NAME).then(cache => {
-              cache.put(event.request, cloneResponse).then(function(){
-
-              })
-            })
-          }else{
-            return response
-          }
-          return response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
         }
-      }).catch(err => {
-        return console.log(err)
-      })
-    })
-  )
-}else {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) {
-        return response
-      }
 
-      return caches.match("onoffline.html")
-      .then(responseNodata => {
-        return responseNodata
-      })
+        let responseToCache = response.clone();
 
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+
+      });
     })
-  )
-}})
+  );
+})
