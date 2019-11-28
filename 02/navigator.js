c1a6c1f9d9d6d9060eca3dbdf3adaa7b82e6d95f
Taka-K <55ko22ni44shi@gmail.com>
2019-11-28 18:31:03 +0900

    簡素化

--- 02/navigator.js
+++ 02/navigator.js
@@ -1,4 +1,13 @@
 // service workerが有効なら、service-worker.js を登録します
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js');
+  navigator.serviceWorker.register('./service-worker.js')
+  .then(function(registration) {
+    if (typeof registration.update == 'function') {
+      registration.update();
+    }
+  })
+  .catch(function(error) {
+    // statements
+    console.log(#EROOR: "" + error);
+  });
 }
--- 02/service-worker.js
+++ 02/service-worker.js
@@ -1,80 +1,46 @@
-'use NONE';
-secure;
-httpsonly;
+// https://qiita.com/OMOIKANESAN/items/5b23fa8ea9ea0d181df5
 
-let CACHE_NAME = 'cache-v1';
+let CACHE_NAME = 'cache-v3';
 let urlsToCache = [
   './',
   './index.html',
   './about.html',
   './contact.html',
   './css/style.css',
-  '//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
   './img/cafe--coffe01.jpg',
   './img/cafe--coffe02.jpg',
   './img/cafe--coffe03.jpg',
   './img/cafe--coffe04.jpg',
   './img/cafe--farmer01.jpg',
-  '//code.jquery.com/jquery-3.3.1.slim.min.js',
-  '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
-  '//stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
+  'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
   './js/index.js'
 ];
 
 // install-event
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
 
         // 指定されたリソースをキャッシュに追加する
-      return cache.addAll(urlsToCache);
-    })
-  )
-});
-
-// activate-event
-self.addEventListener('activate', (event) => {
-  let cacheWhileList = [CACHE_NAME];
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhileList.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+        return cache.addAll(urlsToCache);
     })
   );
 });
 
 // fetch-event
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-
-      let fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest).then((response) => {
-
-        if (!response || response.status !== 200 || response.type !== 'basic') {
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
           return response;
         }
-
-        let responseToCache = response.clone();
-
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseToCache);
-        });
-
-        return response;
-
-      });
-    })
+        return fetch(event.request);
+      })
   );
 })
