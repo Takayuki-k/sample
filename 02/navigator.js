502b1cdc8c8fb593eb906cf6b9d2e5ee1d8a633f
Taka-K <55ko22ni44shi@gmail.com>
2019-12-02 22:00:46 +0900

    arrowFunction

--- 02/navigator.js
+++ 02/navigator.js
@@ -1,5 +1,5 @@
 // service workerが有効なら、service-worker.js を登録します
-window.addEventListener('load', function() {
+window.addEventListener('load', () => {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('service-worker.js')
   }
